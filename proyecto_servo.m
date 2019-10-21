85e0644ccc2697f9af4a71e7a006299ac7098746
iri16009 <iri16009@uvg.edu.gt>
2019-10-21 10:14:32 -0600

    Plot de los datos
    Solo hice la gráfica de las 4 tomas que hicimos.

---
+++ proyecto_servo.m
@@ -0,0 +1,37 @@
+%% IE3006 - Sistemas de Control 1 - Proyecto final
+% Gabriela Iriarte - 16009
+% Eduardo Santizo  - 16089
+%% Captura de datos
+
+% toma1.csv
+subplot(2,2,1)
+plot(t_1,CH1_1)
+hold on;
+plot(t_1,CH2_1)
+
+% toma4.csv
+subplot(2,2,2)
+plot(t_2,CH1_2)
+hold on;
+plot(t_2,CH2_2)
+
+% toma3.csv
+subplot(2,2,3)
+plot(t_3,CH1_3)
+hold on;
+plot(t_3,CH2_3)
+
+% toma2.csv
+subplot(2,2,4)
+plot(t_4,CH1_4)
+hold on;
+plot(t_4,CH2_4)
+
+%% Recorte de datos
+
+
+
+
+
+
+
